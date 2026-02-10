71b4b6d929ec041d0d029106d6b8ec3555056f17
Sabahattin Giritli <sabahattin2005@hotmail.de>
2026-02-10 20:23:26 +0100

    chore: deploy

--- docs/assets/md.transition-legacy-g8xM85cx.js
+++ docs/assets/md.transition-legacy-g8xM85cx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3L2uT9J9.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.g,i=e.c}],execute:function(){
+System.register(["./index-legacy-PTziXo0a.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.g,i=e.c}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
