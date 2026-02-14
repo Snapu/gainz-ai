7bfbb5b8e755ae2b3b8d69af7309402466e0fdcc
Sabahattin Giritli <sabahattin2005@hotmail.de>
2026-02-14 22:00:39 +0100

    hash router works better with github pages

--- docs/assets/ios.transition-legacy-Cnqg3_1x.js
+++ docs/assets/ios.transition-legacy-Cnqg3_1x.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BoxhUBkE.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
+System.register(["./index-legacy-Dqil0YTh.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
