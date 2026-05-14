50b701bcde6441a2967a81087cb763b2fadbe2bd
Snapu <sabahattin2005@hotmail.de>
2026-05-14 19:32:01 +0200

    deploy

---
+++ docs/assets/TrainingInsights-B-5MmKMr.js
@@ -0,0 +1,1 @@
+import{H as e,O as t,S as n,xt as r}from"./UiButton-CxebPqFu.js";import{m as i}from"./index-dSZakTLZ.js";var a=t({__name:`TrainingInsights`,setup(t){return(t,a)=>(e(),n(r(i)))}});export{a as default};
